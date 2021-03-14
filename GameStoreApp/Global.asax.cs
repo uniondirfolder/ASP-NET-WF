375b53deb2fab787c75ef911711302378aa42022
NVV <uniondirfolder@gmail.com>
2021-03-14 12:43:29 -0700

    Настройка проверки достоверности клиентской стороны

--- GameStoreApp/Global.asax.cs
+++ GameStoreApp/Global.asax.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -15,6 +16,7 @@ namespace GameStoreApp
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Session_Start(object sender, EventArgs e)
