827a231682abb19a51c8cac0f3488ac8a2a8ecc2
NVV <uniondirfolder@gmail.com>
2021-03-14 12:20:06 -0700

    Add required atrr in order class

--- GameStoreApp/Models/Order.cs
+++ GameStoreApp/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,18 @@ namespace GameStoreApp.Models
     public class Order
     {
         public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Будьласка введіть своє ім'я")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Ви маєте вказати принаймі одну адресу доставки")]
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Вкажіть місто, куди треба доставити замовлення")]
         public string City { get; set; }
+
         public bool GiftWrap { get; set; }
         public bool Dispatched { get; set; }
         public virtual List<OrderLine> OrderLines { get; set; }
---
+++ Task-1/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Task_1.Default" %>
+
+<!DOCTYPE html>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Вход</title>
+
+</head>
+<body>
+    <div class="container">
+        <form runat="server">
+            
+            
+
+
+        </form>
+     </div>
+   
+</body>
+</html>
